9329325fea0c5c6f13a5d5055a2123c46c5f9992
René Raab <mail@reneraab.org>
2016-01-04 19:53:08 +0100

    Add response param to all callbacks, fixes #30

--- twitter.js
+++ twitter.js
@@ -62,7 +62,7 @@ Twitter.prototype.verifyCredentials = function(accessToken, accessTokenSecret, c
 			try {
 				callback(null, JSON.parse(data));
 			} catch (e) {
-				callback(e, data);
+				callback(e, data, response);
 			}
 		}
 	});
@@ -107,7 +107,7 @@ Twitter.prototype.getTimeline = function(type, params, accessToken, accessTokenS
 			try {
 				callback(null, JSON.parse(data));
 			} catch (e) {
-				callback(e, data);
+				callback(e, data, response);
 			}
 		}
 	});
